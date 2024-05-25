91c9f9e351c61fa7c2774c714bf7a08e2efc3ff8
Ares <areszee7@gmail.com>
2024-05-24 22:28:29 -0700

    Query 2 Implementation - Filter Snippets by Tags in Explore

--- snipscript/src/components/snippet_card.js
+++ snipscript/src/components/snippet_card.js
@@ -1,26 +1,23 @@
-import React, { useEffect } from 'react';
-import { Draggable } from 'react-beautiful-dnd';
-import { useState } from 'react';
-import SnippetForm from './snippet_card_form';
-import ViewSnippet from './snippet_modal';
+import React, { useEffect, useState } from "react";
+import ViewSnippet from "./snippet_modal";
 
 export default function SnippetCard({ snippet }) {
-
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [snippetTags, setSnippetTags] = useState(null);
-
 
   useEffect(() => {
     const fetchTags = async () => {
       try {
-        const response = await fetch(`http://localhost:3001/snippet/tags/${snippet.id}`);
+        const response = await fetch(
+          `http://localhost:3001/snippet/tags/${snippet.id}`
+        );
         if (!response.ok) {
-          throw new Error('Network response was not ok');
+          throw new Error("Network response was not ok");
         }
         const data = await response.json();
         setSnippetTags(data);
       } catch (error) {
-        console.error('Error:', error);
+        console.error("Error:", error);
       }
     };
     fetchTags();
@@ -28,20 +25,43 @@ export default function SnippetCard({ snippet }) {
 
   return (
     <>
-      <div className="bg-white p-4 rounded-xl shadow-lg mb-4 cursor-pointer" onClick={() => setIsModalOpen(true)}>
-          <p className="text-sm text-gray-500 mb-2">{snippet.username}</p>
-          <h4 className="text-xl font-semibold mb-2 truncate">{snippet.title}</h4>
-          <p className="text-sm text-gray-600 mb-2" style={{ overflow: 'hidden', textOverflow: 'ellipsis', 
-            display: '-webkit-box', WebkitLineClamp: 2, WebkitBoxOrient: 'vertical' }}>{snippet.snippet_description}</p>
-          <div className="flex flex-wrap">
-              {snippetTags && (snippetTags.tags.map((tag, index) => (
-                  <span key={index} className="bg-violet-200 text-violet-800 px-2 py-1 rounded-full text-xs mr-2 mb-2" >{tag}</span>
-              )))}
-          </div>
+      <div
+        className="p-4 mb-4 bg-white shadow-lg cursor-pointer rounded-xl"
+        onClick={() => setIsModalOpen(true)}
+      >
+        <p className="mb-2 text-sm text-gray-500">{snippet.username}</p>
+        <h4 className="mb-2 text-xl font-semibold truncate">{snippet.title}</h4>
+        <p
+          className="mb-2 text-sm text-gray-600"
+          style={{
+            overflow: "hidden",
+            textOverflow: "ellipsis",
+            display: "-webkit-box",
+            WebkitLineClamp: 2,
+            WebkitBoxOrient: "vertical",
+          }}
+        >
+          {snippet.snippet_description}
+        </p>
+        <div className="flex flex-wrap">
+          {snippetTags &&
+            snippetTags.tags.map((tag, index) => (
+              <span
+                key={index}
+                className="px-2 py-1 mb-2 mr-2 text-xs rounded-full bg-violet-200 text-violet-800"
+              >
+                {tag}
+              </span>
+            ))}
+        </div>
       </div>
       {isModalOpen && (
-        <ViewSnippet card={snippet} snippetTags={snippetTags} closeModal={() => setIsModalOpen(false)}></ViewSnippet>
+        <ViewSnippet
+          card={snippet}
+          snippetTags={snippetTags}
+          closeModal={() => setIsModalOpen(false)}
+        />
       )}
     </>
   );
-};
+}
--- snipscript/src/pages/explore.js
+++ snipscript/src/pages/explore.js
@@ -2,43 +2,115 @@ import { useState, useEffect } from "react";
 import SnippetCard from "../components/snippet_card";
 
 export default function Explore() {
-    const [snippets, setSnippets] = useState([]);
+  const [snippets, setSnippets] = useState([]);
+  const [allSnippets, setAllSnippets] = useState([]); // Store all snippets for filtering
+  const [tags, setTags] = useState([]);
+  const [selectedTag, setSelectedTag] = useState("");
 
-    const fetchSnippets = () => {
-        fetch(`http://localhost:3001/explore`, {
-            method: 'GET',
-            headers: {
-                'Content-Type': 'application/json',
-            }})
-            .then(response => {
-                if (!response.ok) {
-                    throw new Error('Network response was not ok');
-                }
-                return response.json();
-            })
-            .then(data => {
-                setSnippets(data);
-            })
-            .catch(error => {
-                console.error('Error:', error);
-            });
-    };
+  const fetchSnippets = () => {
+    fetch(`http://localhost:3001/explore`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        return response.json();
+      })
+      .then((data) => {
+        setSnippets(data);
+        setAllSnippets(data); // Store all snippets for filtering
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  };
 
-    useEffect(() => {
-        fetchSnippets();
-    }, []);
+  const fetchTags = () => {
+    fetch(`http://localhost:3001/popular-tags`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        return response.json();
+      })
+      .then((data) => {
+        setTags(data);
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  };
 
-    
-return (
+  const handleTagClick = (tag) => {
+    setSelectedTag(tag);
+    if (tag === "") {
+      setSnippets(allSnippets); // Show all snippets if no tag is selected
+    } else {
+      const filteredSnippets = allSnippets.filter((snippet) =>
+        snippet.tags.includes(tag)
+      );
+      setSnippets(filteredSnippets);
+    }
+  };
+
+  useEffect(() => {
+    fetchSnippets();
+    fetchTags();
+  }, []);
+
+  return (
     <div className="flex flex-col h-full">
-        <h1 className="mt-12 mb-12 text-4xl font-semibold text-center text-white">EXPLORE</h1>
-        <input type="text" name="search" id="search" placeholder="Search snippets" className="w-1/2 place-self-center rounded-3xl h-11 border-0 py-1.5 pr-14 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 sm:text-md sm:leading-6" />
-        <div className="flex items-center justify-center w-full overflow-auto bg-transparent">
-            <div className="grid grid-cols-1 gap-5 p-4 mx-12 mt-12 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 md:p-2 xl:p-5">
-                {snippets.map(snippet => (
-                    <SnippetCard key={snippet.id} snippet={snippet} />
-                ))}
-            </div>
+      <h1 className="mt-12 mb-12 text-4xl font-semibold text-center text-white">
+        EXPLORE
+      </h1>
+      <input
+        type="text"
+        name="search"
+        id="search"
+        placeholder="Search snippets"
+        className="w-1/2 place-self-center rounded-3xl h-11 border-0 py-1.5 pr-14 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 sm:text-md sm:leading-6"
+      />
+      <div className="flex flex-wrap justify-center mt-4">
+        <button
+          className={`m-2 px-4 py-2 rounded-full hover:bg-blue-700 ${
+            selectedTag === ""
+              ? "bg-blue-800 text-white"
+              : "bg-blue-600 text-white"
+          }`}
+          onClick={() => handleTagClick("")}
+        >
+          All
+        </button>
+        {tags.map((tag) => (
+          <button
+            key={tag.tag}
+            className={`m-2 px-4 py-2 rounded-full hover:bg-blue-700 ${
+              selectedTag === tag.tag
+                ? "bg-blue-800 text-white"
+                : "bg-blue-600 text-white"
+            }`}
+            onClick={() => handleTagClick(tag.tag)}
+          >
+            {tag.tag}
+          </button>
+        ))}
+      </div>
+      <div className="flex items-center justify-center w-full overflow-auto bg-transparent">
+        <div className="grid grid-cols-1 gap-5 p-4 mx-12 mt-12 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 md:p-2 xl:p-5">
+          {snippets.map((snippet) => (
+            <SnippetCard key={snippet.id} snippet={snippet} />
+          ))}
         </div>
+      </div>
     </div>
-)};
+  );
+}
